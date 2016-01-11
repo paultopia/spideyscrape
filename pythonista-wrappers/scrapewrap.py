19d6dda158b7fbbc693f93334d7c5abca168f530
Paul Gowder <paultopia@users.noreply.github.com>
2016-01-10 21:36:09 -0400

    Cleanup, make more talkey

--- pythonista-wrappers/scrapewrap.py
+++ pythonista-wrappers/scrapewrap.py
@@ -2,14 +2,16 @@
 # allow you to do something with it, then silently delete the original from pythonista 
 # sandboxed internal filesystem to avoid clutter.
 
+# use with bookmarklet: 
+# javascript:window.location='pythonista://scrapewrap.py?action=run&args='+window.location.href;
+
 import sys
 import spideyscrape
 import console
 import os
 
-args = sys.argv[1:]  # see if the user gave us a command line argument
-start = args[0] if args else raw_input('URL to crawl: ')
-html = spideyscrape.scrape(start)
+print('scraping ' + str(sys.argv[1]))
+html = spideyscrape.scrape(sys.argv[1])
 filename = spideyscrape.savePage(html)
 console.open_in(filename)
 os.remove(filename)
